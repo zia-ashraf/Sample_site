a917b5f31d511345b96d823fdae8e2dfd3bbb02c
Zia <zeazzzz4@gmail.com>
2021-05-23 19:53:23 +0530

    smooth scroll added via jquery

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@ function initMap() {
     zoom: 8,
   });
 }
+
+// adding tranparency to the navbar
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling option
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    const hash = this.hash;
+    $("html,body").animate(
+      {
+        scrollTop: $(hash).offset().top - 65,
+      },
+      800
+    );
+  }
+});
